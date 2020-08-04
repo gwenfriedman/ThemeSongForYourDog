e4e02d10d57a73721790a51e6f894db464eb7dc9
Gwen Friedman <gwenfriedman@Gwens-MacBook-Air.local>
2020-08-04 15:00:59 -0400

    video recording with background music

---
+++ Dog Theme Song/Controller/VideoController.swift
@@ -0,0 +1,97 @@
+//
+//  VideoController.swift
+//  Dog Theme Song
+//
+//  Created by Gwen Friedman on 8/4/20.
+//  Copyright © 2020 Gwen Friedman. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+
+class VideoController: SwiftyCamViewController, SwiftyCamViewControllerDelegate {
+    
+    @IBOutlet weak var captureButton    : SwiftyRecordButton!
+    
+    var drum: AVAudioPlayer = AVAudioPlayer()
+    
+    @IBAction func play(_ sender: Any) {
+        let fileURL = Bundle.main.path(forResource: "drum", ofType: "mp3")!
+        let url = URL(fileURLWithPath: fileURL)
+        do {
+            drum = try AVAudioPlayer(contentsOf: url)
+            drum.play()
+        } catch {
+            // couldn't load file :(
+        }
+        print("play song")
+        }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        shouldPrompToAppSettings = true
+        cameraDelegate = self
+        maximumVideoDuration = 30.0
+        shouldUseDeviceOrientation = true
+        allowAutoRotate = true
+        audioEnabled = true
+        flashMode = .auto
+        captureButton.buttonEnabled = false
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        captureButton.delegate = self
+    }
+    
+    func swiftyCamSessionDidStartRunning(_ swiftyCam: SwiftyCamViewController) {
+        print("Session did start running")
+        captureButton.buttonEnabled = true
+    }
+    
+    func swiftyCamSessionDidStopRunning(_ swiftyCam: SwiftyCamViewController) {
+        print("Session did stop running")
+        captureButton.buttonEnabled = false
+    }
+
+    func swiftyCam(_ swiftyCam: SwiftyCamViewController, didBeginRecordingVideo camera: SwiftyCamViewController.CameraSelection) {
+        print("Did Begin Recording")
+        captureButton.growButton()
+    }
+
+    func swiftyCam(_ swiftyCam: SwiftyCamViewController, didFinishRecordingVideo camera: SwiftyCamViewController.CameraSelection) {
+        print("Did finish Recording")
+        captureButton.shrinkButton()
+    }
+
+    func swiftyCam(_ swiftyCam: SwiftyCamViewController, didFinishProcessVideoAt url: URL) {
+        let newVC = VideoViewController(videoURL: url)
+        self.present(newVC, animated: true, completion: nil)
+    }
+    
+    func swiftyCamDidFailToConfigure(_ swiftyCam: SwiftyCamViewController) {
+        let message = NSLocalizedString("Unable to capture media", comment: "Alert message when something goes wrong during capture session configuration")
+        let alertController = UIAlertController(title: "AVCam", message: message, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Alert OK button"), style: .cancel, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
+
+    func swiftyCam(_ swiftyCam: SwiftyCamViewController, didChangeZoomLevel zoom: CGFloat) {
+        print("Zoom level did change. Level: \(zoom)")
+        print(zoom)
+    }
+
+    func swiftyCam(_ swiftyCam: SwiftyCamViewController, didSwitchCameras camera: SwiftyCamViewController.CameraSelection) {
+        print("Camera did change to \(camera.rawValue)")
+        print(camera)
+    }
+    
+    func swiftyCam(_ swiftyCam: SwiftyCamViewController, didFailToRecordVideo error: Error) {
+        print(error)
+    }
+}
